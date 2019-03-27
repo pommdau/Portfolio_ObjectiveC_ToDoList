d6a2ebc2713705b77f45143395eba1c3a9bd2f73
hikeuchi <hikeuchi@morisawa.co.jp>
2019-03-27 09:39:59 +0900

    Totoの項目データのクラス作成

---
+++ ToDoList/ToDoItem.h
@@ -0,0 +1,28 @@
+//
+//  ToDoItem.h
+//  ToDoList
+//
+//  Created by HIROKI IKEUCHI on 2019/03/27.
+//  Copyright © 2019年 hikeuchi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+// 優先順位
+typedef NS_ENUM (NSUInteger, kPriority) {
+    kPriorityIsHigh,
+    kPriorityIsMiddle,
+    kPriorityisLow,
+    kPriorityIsNil,
+};
+
+@interface ToDoItem : NSObject
+
+@property BOOL      isChecked;  // チェックされていたら完了済み
+@property kPriority priority;   // 優先順位
+@property NSString  *name;      // 項目名
+@property NSString  *place;     // 場所
+@property NSDate    *startDate; // 開始日
+@property NSDate    *limitDate; // 期限日
+
+@end
---
+++ ToDoList/ToDoItem.m
@@ -0,0 +1,26 @@
+//
+//  ToDoItem.m
+//  ToDoList
+//
+//  Created by HIROKI IKEUCHI on 2019/03/27.
+//  Copyright © 2019年 hikeuchi. All rights reserved.
+//
+
+#import "ToDoItem.h"
+
+@implementation ToDoItem
+
+- (id)init {
+    self = [super init];
+    if (self) {
+        _isChecked = NO;
+        _priority  = kPriorityIsMiddle;
+        _name      = @"new item";
+        _place     = @"place";
+        _startDate = [NSDate date];
+        _limitDate = [NSDate date];
+    }
+    return self;
+}
+
+@end
