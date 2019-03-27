a38e99e80c3a894bf7934426b055319c79a64925
hikeuchi <hikeuchi@morisawa.co.jp>
2019-03-27 09:06:58 +0900

    Initial Commit

---
+++ ToDoList/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ToDoList
+//
+//  Created by HIROKI IKEUCHI on 2019/03/27.
+//  Copyright © 2019年 hikeuchi. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
